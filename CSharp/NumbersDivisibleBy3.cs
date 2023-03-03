d6c403c4c2a3092794f80d197d6f7c4e7675cf72
Alberto Chirinos Local <developernet@outlook.com>
2023-03-03 11:06:23 -0500

    The class 'NumbersDivisibleBy3' was added to the project.

---
+++ CSharp/NumbersDivisibleBy3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class NumbersDivisibleBy3
+    {
+        internal static void GetNumbersDivisibleBy3()
+        {
+            int i = 1;
+
+            while (i <= 100)
+            {
+                if (IsDivisibleBy3(i))
+                {
+                    Console.WriteLine(i);
+                }
+
+                i++;
+            }
+        }
+        static bool IsDivisibleBy3(int number)
+        {
+            if (number % 3 == 0) return true; else return false;
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,24 +4,33 @@
     {
         static void Main(string[] args)
         {
+            const string line = "---------------------------------------------";
             short option;
 
+        Begin:
             Console.WriteLine("Select an option:");
 
             Console.WriteLine("1    SwitchCase");
             Console.WriteLine("2    PairNumbers");
+            Console.WriteLine("3    NumbersDivisibleBy3");
 
             option = Convert.ToInt16(Console.ReadLine());
 
             switch (option)
             {
                 case 1:
+                    Console.WriteLine(line);
                     Console.WriteLine(SwitchCase.Selection());
                     break;
                 case 2:
+                    Console.WriteLine(line);
                     PairNumbers.GetEvenNumbers();
                     break;
-                default: Console.WriteLine("Select a valid option"); break;
+                case 3:
+                    Console.WriteLine(line);
+                    NumbersDivisibleBy3.GetNumbersDivisibleBy3();
+                    break;
+                default: Console.WriteLine("Select a valid option"); goto Begin;
             }
 
             Console.ReadLine();
