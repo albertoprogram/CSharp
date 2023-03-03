842ec76f314289bb2e72aa032918a7e1591c55b8
Alberto Chirinos Local <developernet@outlook.com>
2023-03-03 10:47:58 -0500

    The class 'PairNumbers' was added to the project

---
+++ CSharp/PairNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class PairNumbers
+    {
+        internal static void GetEvenNumbers()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (IsEven(i))
+                {
+                    Console.WriteLine(i.ToString());
+                }
+
+            }
+        }
+        static bool IsEven(int number)
+        {
+            if ((number % 2) == 0) return true; else return false;
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SwitchCase.Selection());
+            short option;
+
+            Console.WriteLine("Select an option:");
+
+            Console.WriteLine("1    SwitchCase");
+            Console.WriteLine("2    PairNumbers");
+
+            option = Convert.ToInt16(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(SwitchCase.Selection());
+                    break;
+                case 2:
+                    PairNumbers.GetEvenNumbers();
+                    break;
+                default: Console.WriteLine("Select a valid option"); break;
+            }
 
             Console.ReadLine();
         }
