361b2b455a5adb95ef1bc6e52f3af3848144a713
Alberto Chirinos Local <developernet@outlook.com>
2023-03-10 08:41:43 -0500

    The class 'PrimeNumber' was added to the project.

---
+++ CSharp/PrimeNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class PrimeNumber
+    {
+        internal static void CheckIfNumberIsPrime()
+        {
+            try
+            {
+                Console.WriteLine("*** A number is prime only when it can be divided exactly by itself and by 1 ***");
+                Console.WriteLine(Program.line);
+                Console.WriteLine("Enter a number greater than 1");
+
+                int? number = Convert.ToInt32(Console.ReadLine());
+
+                if (!number.HasValue || number <= 1)
+                {
+                    Console.WriteLine("invalid number.");
+                    return;
+                }
+
+                for (int i = 2; i < number; i++)
+                {
+                    if ((number % i) == 0)
+                    {
+                        Console.WriteLine($"The number {number} is not prime");
+                        return;
+                    }
+                }
+
+                Console.WriteLine($"The number {number} is prime");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+            finally { ContinueInCurrentFunction(); }
+        }
+
+        static void ContinueInCurrentFunction()
+        {
+            try
+            {
+                char? toContinue;
+
+                Console.WriteLine(Program.line);
+                Console.WriteLine("To continue in this function press Y, or press any other key to return to the menu");
+
+                toContinue = Convert.ToChar(Console.ReadLine().ToUpper());
+
+                if (toContinue == 'Y') CheckIfNumberIsPrime(); else Program.Main(new string[] { });
+            }
+            catch { Program.Main(new string[] { }); };
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -2,55 +2,71 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        internal const string line = "---------------------------------------------";
+        internal static void Main(string[] args)
         {
-            const string line = "---------------------------------------------";
-            short option;
-
-        Begin:
-            Console.WriteLine("Select an option:");
-
-            Console.WriteLine("1    SwitchCase");
-            Console.WriteLine("2    PairNumbers");
-            Console.WriteLine("3    NumbersDivisibleBy3");
-            Console.WriteLine("4    Arrays");
-            Console.WriteLine("5    CalculateAdditionEvenAndSubtractionOdd");
-            Console.WriteLine("6    ExecuteSumAndAverage");
-
-            option = Convert.ToInt16(Console.ReadLine());
-
-            switch (option)
+            try
             {
-                case 1:
-                    Console.WriteLine(line);
-                    Console.WriteLine(SwitchCase.Selection());
-                    Console.WriteLine(line);
-                    SwitchCase.GetTheDayOfWeek();
-                    break;
-                case 2:
-                    Console.WriteLine(line);
-                    PairNumbers.GetEvenNumbers();
-                    break;
-                case 3:
-                    Console.WriteLine(line);
-                    NumbersDivisibleBy3.GetNumbersDivisibleBy3();
-                    break;
-                case 4:
-                    Console.WriteLine(line);
-                    Arrays.ArrayOfEvenNumbers();
-                    break;
-                case 5:
-                    Console.WriteLine(line);
-                    AdditionEvenSubtractionOdd.CalculateAdditionEvenAndSubtractionOdd();
-                    break;
-                case 6:
-                    Console.WriteLine(line);
-                    SumAndAverage.ExecuteSumAndAverage();
-                    break;
-                default: Console.WriteLine("Select a valid option"); goto Begin;
-            }
+                short? option;
+                char? toContinue;
 
-            Console.ReadLine();
+            Begin:
+                Console.WriteLine("Select an option:");
+
+                Console.WriteLine("1    SwitchCase");
+                Console.WriteLine("2    PairNumbers");
+                Console.WriteLine("3    NumbersDivisibleBy3");
+                Console.WriteLine("4    Arrays");
+                Console.WriteLine("5    CalculateAdditionEvenAndSubtractionOdd");
+                Console.WriteLine("6    ExecuteSumAndAverage");
+                Console.WriteLine("7    PrimeNumber");
+                Console.WriteLine(line);
+
+                option = Convert.ToInt16(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(line);
+                        Console.WriteLine(SwitchCase.Selection());
+                        Console.WriteLine(line);
+                        SwitchCase.GetTheDayOfWeek();
+                        break;
+                    case 2:
+                        Console.WriteLine(line);
+                        PairNumbers.GetEvenNumbers();
+                        break;
+                    case 3:
+                        Console.WriteLine(line);
+                        NumbersDivisibleBy3.GetNumbersDivisibleBy3();
+                        break;
+                    case 4:
+                        Console.WriteLine(line);
+                        Arrays.ArrayOfEvenNumbers();
+                        break;
+                    case 5:
+                        Console.WriteLine(line);
+                        AdditionEvenSubtractionOdd.CalculateAdditionEvenAndSubtractionOdd();
+                        break;
+                    case 6:
+                        Console.WriteLine(line);
+                        SumAndAverage.ExecuteSumAndAverage();
+                        break;
+                    case 7:
+                        Console.WriteLine(line);
+                        PrimeNumber.CheckIfNumberIsPrime();
+                        break;
+                    default: Console.WriteLine("Select a valid option"); goto Begin;
+                }
+
+                Console.WriteLine(line);
+                Console.WriteLine("To return to the menu press Y, or press any other key to end the program");
+
+                toContinue = Convert.ToChar(Console.ReadLine().ToUpper());
+
+                if (toContinue == 'Y') goto Begin; else return; ;
+            }
+            catch (Exception) { return; }
         }
     }
 }
